a6d528e7ceb80a92c0f77ecf9ebd7bede978c143
Siarhei Balonikau <siarhei_balonikau@epam.com>
2024-07-19 19:48:59 +0400

    feature/EPMGCIP-151/add-image-zooming

--- jest.config.ts
+++ jest.config.ts
@@ -1,3 +1,5 @@
+const esModules = ['yet-another-react-lightbox'].join('|')
+
 module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.{ts,tsx}'],
@@ -7,11 +9,14 @@ module.exports = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/fileTransformer.js',
   },
-  transformIgnorePatterns: ['/node_modules/'],
+  transformIgnorePatterns: [`/node_modules/(?!${esModules})`],
   testMatch: ['**/src/**/*.test.(ts|tsx|js|jsx)'],
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
   preset: 'ts-jest/presets/default-esm',
   setupFiles: ['./setupTests.ts'],
   modulePathIgnorePatterns: ['src/constants/'],
+  transform: {
+    [`(${esModules}).+\\.js$`]: 'babel-jest',
+  },
 }
