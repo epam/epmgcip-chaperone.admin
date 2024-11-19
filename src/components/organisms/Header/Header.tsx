baad64b75648a94e687c047c89ee871c64efe4b9
Dzmitry Yaniuk <dzmitry_yaniuk@epam.com>
2024-11-19 23:11:59 +0300

    EPMGCIP-171: Adjust "Header" component to make it logo clickable

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,15 +1,25 @@
-import { useTranslations } from "next-intl";
+"use client";
+
+import { useLocale, useTranslations } from "next-intl";
 import Image from "next/image";
 import styles from "./Header.module.scss";
 import LanguageSwitcher from "@/components/molecules/LanguageSwitcher/LanguageSwitcher";
 import logo from "@/assets/image/logo.png";
+import { useRouter } from "next/navigation";
 
 export default function Header() {
+  const router = useRouter();
+  const locale = useLocale();
   const t = useTranslations();
+
+  const onClickLogo = (): void => {
+    router.push(`/${locale}`);
+  };
 
   return (
     <div className={styles.header} data-testid="header-component">
-      <Image src={logo} width={68} alt={t("logo")} />
+      <Image src={logo} width={68} alt={t("logo")} className={styles.logo} onClick={onClickLogo} />
+
       <LanguageSwitcher />
     </div>
   );
