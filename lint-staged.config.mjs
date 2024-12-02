690ccef9b3449a15d9ccf0509110c4e9081da88f
Dzmitry Yaniuk <dzmitry_yaniuk@epam.com>
2024-12-02 23:30:45 +0300

    EPMGCIP-179: Decrease allowed amount of ESLint warnings from 30 to 20

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,5 +1,5 @@
 const config = {
-  '*.{js,jsx,ts,tsx}': ['eslint --fix --max-warnings 30', 'prettier --write'],
+  '*.{js,jsx,ts,tsx}': ['eslint --fix --max-warnings 20', 'prettier --write'],
   '*.{json,scss,css}': ['prettier --write'],
 };
 
