9f33b772f629c3e1fdd50abdf36f5133895136d3
Dzmitry Yaniuk <dzmitry_yaniuk@epam.com>
2024-12-22 13:02:34 +0300

    EPMGCIP-163: Improve UTs for "Header" to check new scenarios

--- src/components/organisms/Header/Header.test.tsx
+++ src/components/organisms/Header/Header.test.tsx
@@ -1,6 +1,9 @@
+import React from 'react';
+
 import '@testing-library/jest-dom';
 
-import { screen } from '@testing-library/react';
+import { MantineProvider } from '@mantine/core';
+import { screen, render, fireEvent, waitFor } from '@testing-library/react';
 import { NextIntlClientProvider } from 'next-intl';
 
 import messages from 'messages/en.json';
@@ -11,15 +14,16 @@ import { ILink } from '@/interfaces/ILink';
 import Header from './Header';
 
 jest.mock('@/navigation', () => ({
-  Link: jest.fn().mockReturnValue(<img src={'/test'} alt={'test'} />),
+  __esModule: true,
+  ...jest.requireActual('@/navigation'),
   usePathname: jest.fn().mockReturnValue('/test'),
   useRouter: jest.fn().mockReturnValue({
     replace: jest.fn(),
   }),
 }));
 
-jest.mock('@/hooks', () => ({
-  useMobileView: jest.fn().mockReturnValue(false),
+jest.mock('@/hooks/use-mobile-view', () => ({
+  useMobileView: jest.fn(),
 }));
 
 const testRoutes: ILink[] = [
@@ -51,15 +55,19 @@ const testRoutes: ILink[] = [
   },
 ];
 
+const useMobileViewMock = useMobileView as jest.Mock;
+
 describe('Header', () => {
   const renderComponent = (isMobile: boolean, links: ILink[]) => {
-    (useMobileView as jest.Mock).mockReturnValue(isMobile);
+    useMobileViewMock.mockReturnValue(isMobile);
 
-    return (
-      <NextIntlClientProvider locale="en" messages={messages}>
-        <Header links={links} />
-      </NextIntlClientProvider>
-    );
+    return render(<Header links={links} />, {
+      wrapper: ({ children }: { children: React.ReactNode }) => (
+        <NextIntlClientProvider locale="en" messages={messages}>
+          <MantineProvider>{children}</MantineProvider>
+        </NextIntlClientProvider>
+      ),
+    });
   };
 
   afterEach(() => {
@@ -67,8 +75,70 @@ describe('Header', () => {
   });
 
   it('should render component with related active links in desktop view', () => {
-    renderComponent(false, testRoutes);
+    const appRoutes = [...testRoutes];
+    appRoutes[1].isEnabled = false;
 
-    expect(screen.getByTestId('header-component')).toBeInTheDocument();
+    const expectedActiveLinksAmount = 2;
+
+    renderComponent(false, appRoutes);
+
+    const mainHeaderComponent = screen.getByTestId('header-component');
+
+    expect(mainHeaderComponent).toBeInTheDocument();
+
+    const displayedLinks: HTMLAnchorElement[] = screen.getAllByTestId('link');
+
+    expect(displayedLinks.length).toBe(expectedActiveLinksAmount);
+
+    expect(useMobileViewMock).toHaveBeenCalled();
+  });
+
+  it('should render component with related active links in mobile view', async () => {
+    const appRoutes = [...testRoutes];
+    appRoutes[1].isEnabled = false;
+
+    const expectedActiveLinksAmount = 2;
+
+    renderComponent(true, appRoutes);
+
+    const mainHeaderComponent = screen.getByTestId('header-component');
+
+    expect(mainHeaderComponent).toBeInTheDocument();
+
+    const mobileMenuButton = screen.getByTestId('mobile-menu');
+
+    expect(mobileMenuButton).toBeInTheDocument();
+
+    fireEvent.click(mobileMenuButton);
+
+    await waitFor(() => {
+      const displayedLinks: HTMLAnchorElement[] = screen.getAllByTestId('link');
+
+      expect(displayedLinks.length).toBe(expectedActiveLinksAmount);
+    });
+
+    expect(useMobileViewMock).toHaveBeenCalled();
+  });
+
+  it('should close mobile sidebar on clicking close button', async () => {
+    renderComponent(true, testRoutes);
+
+    const mobileMenuButton = screen.getByTestId('mobile-menu');
+
+    fireEvent.click(mobileMenuButton);
+
+    await waitFor(() => {
+      const closeButton = screen.getByTestId('close-drawer-button');
+
+      expect(closeButton).toBeInTheDocument();
+
+      fireEvent.click(closeButton);
+    });
+
+    await waitFor(() => {
+      const displayedLinks = screen.queryAllByTestId('link');
+
+      expect(displayedLinks).toEqual([]);
+    });
   });
 });
