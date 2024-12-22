9fc9854af421bcc10a763a568a87c8ea58e51eb0
Dzmitry Yaniuk <dzmitry_yaniuk@epam.com>
2024-12-22 13:35:05 +0300

    dx: Update middleware configuration with latest recommended Next settings

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,8 @@
 import createMiddleware from 'next-intl/middleware';
 
-import { ENGLISH_LANGUAGE_CODE, locales } from './locales';
+import { routing } from './i18n/routing';
 
-export default createMiddleware({
-  defaultLocale: ENGLISH_LANGUAGE_CODE,
-  locales: locales,
-});
+export default createMiddleware(routing);
 
 export const config = {
   matcher: ['/', '/((?!_next/static|images|svg|favicon.ico).*)'],
