02c0fdda838227672e1f43b840de8b904c1426b7
Dzmitry Yaniuk <dzmitry_yaniuk@epam.com>
2025-01-06 12:10:42 +0300

    EPMGCIP-181: Resolve "ContactForm" UTs bad setup for Captcha, update usages

--- src/components/forms/ContactForm/ContactForm.test.tsx
+++ src/components/forms/ContactForm/ContactForm.test.tsx
@@ -1,8 +1,10 @@
+import React, { forwardRef, useEffect, useImperativeHandle } from 'react';
+
 import { MantineProvider } from '@mantine/core';
-import { act, render, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { screen, render, waitFor, fireEvent } from '@testing-library/react';
 import { NextIntlClientProvider } from 'next-intl';
-import ReCAPTCHA from 'react-google-recaptcha';
+
+import messages from 'messages/en.json';
 
 import { sendContactForm } from '@/actions';
 import { NotificationType } from '@/enums';
@@ -10,9 +12,36 @@ import { useShowNotification } from '@/hooks';
 
 import ContactForm from './ContactForm';
 
+/* eslint-disable max-nested-callbacks */
+
 jest.mock('react-google-recaptcha', () => {
-  return jest.fn().mockImplementation(() => 'ReCAPTCHA');
+  return forwardRef(function Component(
+    props: { asyncScriptOnLoad: () => void; onChange: (token: string) => void },
+    ref,
+  ) {
+    useImperativeHandle(ref, () => ({
+      asyncScriptOnLoad: jest.fn(() => {}),
+      execute: jest.fn(),
+      executeAsync: jest.fn(() => 'test-token'),
+      onChange: jest.fn(),
+      reset: jest.fn(),
+    }));
+
+    useEffect(() => {
+      props?.asyncScriptOnLoad();
+      props?.onChange('test-token');
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars, react/prop-types
+    const { asyncScriptOnLoad, ...domProps } = props;
+
+    // @ts-expect-error: Skip TS ref type incompatibilities.
+    return <input {...domProps} ref={ref} type="checkbox" data-testid="mock-v2-captcha-element" />;
+  });
 });
+
+/* eslint-enable max-nested-callbacks */
 
 jest.mock('@/actions', () => ({
   sendContactForm: jest.fn(() => Promise.resolve({ success: true })),
@@ -21,114 +50,114 @@ jest.mock('@/hooks', () => ({
   useShowNotification: jest.fn(() => jest.fn()),
 }));
 
+const useShowNotificationMock = useShowNotification as jest.Mock;
+const sendContactFormMock = sendContactForm as jest.Mock;
+
 const defaultProps = {
   reCaptchaSiteKey: 'test-site-key',
 };
 
-const wrapper = ({ children }: { children: React.ReactNode }) => (
-  <NextIntlClientProvider locale="en">
-    <MantineProvider>{children}</MantineProvider>
-  </NextIntlClientProvider>
-);
-
-const renderComponent = (props = {}) =>
-  render(<ContactForm {...defaultProps} {...props} />, { wrapper });
-
-const solveCaptcha = () =>
-  act(async () => {
-    (ReCAPTCHA as jest.Mock).mock.calls[0][0].onChange('test-token');
-  });
-
 describe('ContactForm', () => {
+  const wrapper = ({ children }: { children: React.ReactNode }) => (
+    <NextIntlClientProvider locale="en" messages={messages}>
+      <MantineProvider>{children}</MantineProvider>
+    </NextIntlClientProvider>
+  );
+
+  const renderComponent = (props = {}) =>
+    render(<ContactForm {...defaultProps} {...props} />, { wrapper });
+
   afterEach(() => {
     jest.clearAllMocks();
   });
 
-  it('should render form', () => {
-    const { getByText } = renderComponent();
+  it('should render form', async () => {
+    renderComponent();
 
-    expect(getByText('contactForm.title')).toBeInTheDocument();
-    expect(getByText('contactForm.fields.name.label')).toBeInTheDocument();
-    expect(getByText('contactForm.fields.email.label')).toBeInTheDocument();
-    expect(getByText('contactForm.fields.subject.label')).toBeInTheDocument();
-    expect(getByText('contactForm.fields.message.label')).toBeInTheDocument();
-    expect(getByText('ReCAPTCHA')).toBeInTheDocument();
+    expect(screen.getByText('Feedback form')).toBeInTheDocument();
+    expect(screen.getByText('Name')).toBeInTheDocument();
+    expect(screen.getByText('E-mail')).toBeInTheDocument();
+    expect(screen.getByText('Subject')).toBeInTheDocument();
+    expect(screen.getByText('Message')).toBeInTheDocument();
+
+    await waitFor(() => {
+      expect(screen.getByTestId('mock-v2-captcha-element')).toBeInTheDocument();
+    });
   });
 
   it('should show validation error if form is submitted without filling all required fields', async () => {
-    const { getByText, getByRole } = renderComponent();
+    renderComponent();
 
-    await solveCaptcha();
-
-    await userEvent.type(
-      getByRole('textbox', { name: 'contactForm.fields.name.label' }),
-      'test-name',
-    );
-    await userEvent.click(getByRole('button', { name: 'submit' }));
+    fireEvent.change(screen.getByTestId('contact-name'), { target: { value: 'test-name' } });
+    fireEvent.click(screen.getByTestId('contact-submit-button'));
 
     await waitFor(() => {
-      expect(getByText('contactForm.fields.email.validation')).toBeInTheDocument();
-      expect(getByText('contactForm.fields.subject.validation')).toBeInTheDocument();
-      expect(getByText('contactForm.fields.message.validation')).toBeInTheDocument();
+      expect(screen.getByText('E-mail is required')).toBeInTheDocument();
+      expect(screen.getByText('Select subject')).toBeInTheDocument();
+      expect(screen.getByText('Message is required')).toBeInTheDocument();
     });
   });
 
-  it('should submit form if all required fields are filled and ReCAPTCHA token is provided', async () => {
-    const { getByRole } = renderComponent();
-
-    await solveCaptcha();
-
-    await userEvent.type(
-      getByRole('textbox', { name: 'contactForm.fields.name.label' }),
-      'test-name',
-    );
-    await userEvent.type(
-      getByRole('textbox', { name: 'contactForm.fields.email.label' }),
-      'ex@mp.le',
-    );
-    await userEvent.click(getByRole('textbox', { name: 'contactForm.fields.subject.label' }));
-    await userEvent.click(getByRole('option', { name: 'contactForm.subjects.other' }));
-    await userEvent.type(
-      getByRole('textbox', { name: 'contactForm.fields.message.label' }),
-      'test-message',
-    );
-
-    await userEvent.click(getByRole('button', { name: 'submit' }));
-
-    expect(sendContactForm).toHaveBeenCalledWith({
-      email: 'ex@mp.le',
-      message: 'test-message',
-      name: 'test-name',
-      subject: 'contactForm.subjects.other',
-    });
-  });
-
-  it('should handle send contact form action failure', async () => {
+  it('should handle send contact form action success if all required fields are filled and ReCAPTCHA token is provided', async () => {
     const showNotification = jest.fn();
 
-    (useShowNotification as jest.Mock).mockReturnValue(showNotification);
-    (sendContactForm as jest.Mock).mockReturnValue({ messages: ['test-error'], success: false });
+    useShowNotificationMock.mockReturnValue(showNotification);
 
-    const { getByRole } = renderComponent();
+    renderComponent();
 
-    await solveCaptcha();
+    fireEvent.change(screen.getByTestId('contact-name'), {
+      target: { value: 'test-name' },
+    });
+    fireEvent.change(screen.getByTestId('contact-email'), { target: { value: 'test@mail.com' } });
+    fireEvent.change(screen.getByTestId('contact-message'), { target: { value: 'test-message' } });
 
-    await userEvent.type(
-      getByRole('textbox', { name: 'contactForm.fields.name.label' }),
-      'test-name',
-    );
-    await userEvent.type(
-      getByRole('textbox', { name: 'contactForm.fields.email.label' }),
-      'ex@mp.le',
-    );
-    await userEvent.click(getByRole('textbox', { name: 'contactForm.fields.subject.label' }));
-    await userEvent.click(getByRole('option', { name: 'contactForm.subjects.other' }));
-    await userEvent.type(
-      getByRole('textbox', { name: 'contactForm.fields.message.label' }),
-      'test-message',
-    );
+    fireEvent.click(screen.getByPlaceholderText('Select subject'));
+    fireEvent.click(screen.getByText('Cooperation'));
 
-    await userEvent.click(getByRole('button', { name: 'submit' }));
+    fireEvent.click(screen.getByTestId('contact-submit-button'));
+
+    await waitFor(() => {
+      expect(sendContactForm).toHaveBeenCalledWith({
+        email: 'test@mail.com',
+        message: 'test-message',
+        name: 'test-name',
+        subject: 'Cooperation',
+      });
+    });
+
+    expect(showNotification).toHaveBeenCalledWith({
+      message: 'Email sent successfully',
+      type: NotificationType.Success,
+    });
+  });
+
+  it('should handle send contact form action failure if all required fields are filled and ReCAPTCHA token is provided', async () => {
+    const showNotification = jest.fn();
+
+    useShowNotificationMock.mockReturnValue(showNotification);
+    sendContactFormMock.mockReturnValue({ messages: ['test-error'], success: false });
+
+    renderComponent();
+
+    fireEvent.change(screen.getByTestId('contact-name'), {
+      target: { value: 'test-name' },
+    });
+    fireEvent.change(screen.getByTestId('contact-email'), { target: { value: 'test@mail.com' } });
+    fireEvent.change(screen.getByTestId('contact-message'), { target: { value: 'test-message' } });
+
+    fireEvent.click(screen.getByPlaceholderText('Select subject'));
+    fireEvent.click(screen.getByText('Cooperation'));
+
+    fireEvent.click(screen.getByTestId('contact-submit-button'));
+
+    await waitFor(() => {
+      expect(sendContactForm).toHaveBeenCalledWith({
+        email: 'test@mail.com',
+        message: 'test-message',
+        name: 'test-name',
+        subject: 'Cooperation',
+      });
+    });
 
     expect(showNotification).toHaveBeenCalledWith({
       message: 'test-error',
