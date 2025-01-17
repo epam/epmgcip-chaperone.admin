5429aba5e6c469bc079e6c335eb1d83e24a1e7f9
Siarhei Balonikau <siarhei_balonikau@epam.com>
2025-01-17 19:26:38 +0400

    feature/EPMGCIP-166/Fix prettier issues

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -2,17 +2,15 @@ import { ESLint } from 'eslint';
 
 const removeIgnoredFiles = async (files) => {
   const eslint = new ESLint();
-  const ignoredFiles = await Promise.all(
-    files.map((file) => eslint.isPathIgnored(file)),
-  )
+  const ignoredFiles = await Promise.all(files.map((file) => eslint.isPathIgnored(file)));
   const filteredFiles = files.filter((_, i) => !ignoredFiles[i]);
   return filteredFiles.join(' ');
-}
+};
 
 const config = {
   '*.{js,jsx,ts,tsx}': async (files) => {
-    const filesToLint = await removeIgnoredFiles(files)
-    return [`eslint --fix --max-warnings 0 ${filesToLint}`]
+    const filesToLint = await removeIgnoredFiles(files);
+    return [`eslint --fix --max-warnings 0 ${filesToLint}`];
   },
   '*.{js,jsx,ts,tsx,json,scss,css}': ['prettier --write'],
 };
