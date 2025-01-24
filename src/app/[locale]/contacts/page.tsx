15a33a3265f8d86018fe72210fad073899c93a66
Siarhei Balonikau <siarhei_balonikau@epam.com>
2025-01-24 14:22:41 +0400

    feature/EPMGCIP-166/Add contacts page

--- src/app/[locale]/contacts/page.tsx
+++ src/app/[locale]/contacts/page.tsx
@@ -1,5 +1,16 @@
 import { ContactForm } from '@/components/forms';
+import Page from '@/components/pages/Page/Page';
+import { getPage } from '@/lib/page';
+import getLastUrlSegment from '@/utils/getLastUrlSegment';
 
-export default function ContactsPage() {
-  return <ContactForm reCaptchaSiteKey={process.env.RE_CAPTCHA_SITE_KEY!} />;
+export default async function ContactsPage() {
+  const lastUrlSegment = getLastUrlSegment();
+  const page = await getPage(lastUrlSegment);
+
+  return (
+    <>
+      {page && <Page page={page} />}
+      <ContactForm reCaptchaSiteKey={process.env.RE_CAPTCHA_SITE_KEY!} />
+    </>
+  );
 }
