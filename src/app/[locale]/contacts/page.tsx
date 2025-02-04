f95a6572856976412abd3cb24f1952bdaefaaedb
Siarhei Balonikau <siarhei_balonikau@epam.com>
2025-02-04 18:35:08 +0400

    feature/EPMGCIP-167/Add map

--- src/app/[locale]/contacts/page.tsx
+++ src/app/[locale]/contacts/page.tsx
@@ -1,5 +1,13 @@
+import { Map } from '@/components/atoms/Map/Map';
 import { ContactForm } from '@/components/forms';
 
+const mapHeight = '450';
+
 export default function ContactsPage() {
-  return <ContactForm reCaptchaSiteKey={process.env.RE_CAPTCHA_SITE_KEY!} />;
+  return (
+    <>
+      <Map height={mapHeight} />
+      <ContactForm reCaptchaSiteKey={process.env.RE_CAPTCHA_SITE_KEY!} />
+    </>
+  );
 }
---
+++ src/components/atoms/Map/Map.tsx
@@ -0,0 +1,12 @@
+'use client';
+import styles from './Map.module.scss';
+
+interface Props {
+  height: string;
+}
+
+export const Map = ({ height }: Props) => {
+  return (
+    <iframe className={styles.iframe} height={height} src={process.env.EMBED_MAP_SRC}></iframe>
+  );
+};
